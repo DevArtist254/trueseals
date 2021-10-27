54b84ef0f5ba1adec3140b435848b8df56671d52
edsan <jackwest415@gmail.com>
2021-10-27 06:17:12 +0300

    e

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-console.log("Hello world!");
+console.log("Hello world!")
 
-const myName = "Jonas Schmedtmann";
-const h1 = document.querySelector(".heading-primary");
-console.log(myName);
-console.log(h1);
+const myName = "Jonas Schmedtmann"
+const h1 = document.querySelector(".heading-primary")
+console.log(myName)
+console.log(h1)
 
 // h1.addEventListener("click", function () {
 //   h1.textContent = myName;
@@ -13,65 +13,65 @@ console.log(h1);
 
 ///////////////////////////////////////////////////////////
 // Set current year
-const yearEl = document.querySelector(".year");
-const currentYear = new Date().getFullYear();
-yearEl.textContent = currentYear;
+const yearEl = document.querySelector(".year")
+const currentYear = new Date().getFullYear()
+yearEl.textContent = currentYear
 
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
 
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
+const btnNavEl = document.querySelector(".btn-mobile-nav")
+const headerEl = document.querySelector(".header")
 
 btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
+  headerEl.classList.toggle("nav-open")
+})
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
 
-const allLinks = document.querySelectorAll("a:link");
+// const allLinks = document.querySelectorAll("a:link");
 
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const href = link.getAttribute("href");
 
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
+//     // Scroll back to top
+//     if (href === "#")
+//       window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//       });
 
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
+//     // Scroll to other links
+//     if (href !== "#" && href.startsWith("#")) {
+//       const sectionEl = document.querySelector(href);
+//       sectionEl.scrollIntoView({ behavior: "smooth" });
+//     }
 
-    // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
-  });
-});
+//     // Close mobile naviagtion
+//     if (link.classList.contains("main-nav-link"))
+//       headerEl.classList.toggle("nav-open");
+//   });
+// });
 
 ///////////////////////////////////////////////////////////
 // Sticky navigation
 
-const sectionHeroEl = document.querySelector(".section-hero");
+const sectionHeroEl = document.querySelector(".section-hero")
 
 const obs = new IntersectionObserver(
   function (entries) {
-    const ent = entries[0];
-    console.log(ent);
+    const ent = entries[0]
+    console.log(ent)
 
     if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
+      document.body.classList.add("sticky")
     }
 
     if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
+      document.body.classList.remove("sticky")
     }
   },
   {
@@ -80,28 +80,28 @@ const obs = new IntersectionObserver(
     threshold: 0,
     rootMargin: "-80px",
   }
-);
-obs.observe(sectionHeroEl);
+)
+obs.observe(sectionHeroEl)
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+  var flex = document.createElement("div")
+  flex.style.display = "flex"
+  flex.style.flexDirection = "column"
+  flex.style.rowGap = "1px"
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"))
+  flex.appendChild(document.createElement("div"))
 
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
-  console.log(isSupported);
+  document.body.appendChild(flex)
+  var isSupported = flex.scrollHeight === 1
+  flex.parentNode.removeChild(flex)
+  console.log(isSupported)
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add("no-flexbox-gap")
 }
-checkFlexGap();
+checkFlexGap()
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
